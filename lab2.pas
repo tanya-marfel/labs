78692fe411e3e94ce823f2cd7b52c8cf5d392e83
Tatsiana Marfel <tatsiana_marfel@epam.com>
2018-05-11 10:35:39 +0300

    Fix array element adding logic

--- lab2.pas
+++ lab2.pas
@@ -4,34 +4,44 @@
  * Date: 5/8/18
  *)
 program lab2;
+
+uses regexpr;
+
 var
+  re : TRegExpr;
   s1, s2, ls : string;
-  la : array [1..300] of string;
-  i, minLength : Integer;
+  la : array of string;
+  i, minLength, ind : Integer;
 
 begin
-  ls := 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua';
+  ReadLn(ls);
+  if Length(ls) < 1 then
+    WriteLn('Error. The string should not be empty');
+  re := TRegExpr.Create('[\s]{1,2}');
+  SetLength(la, 90);
+  ind := 1;
   for i:= 1 to Length(ls) do
   begin
-    if NOT(ls[i] = ' ') then
+    if NOT(re.exec(ls[i])) then
     begin
       s1 := Concat(s1 + ls[i]);
-      WriteLn(s1)
     end
     else
     begin
-      la[i] := s1;
+      la[ind] := s1;
       s1 := '';
-      WriteLn(la[i])
+      WriteLn(Length(la[ind]));
+      ind:=ind+1;
     end;
+
   end;
-  minLength := Length(la[1]);
+  minLength:=Length(la[1]);
+  WriteLn('minLength', minLength);
   for i:=1 to Length(la) do
-  begin
-    If (Length(la[i]) < minLength) AND (la[i] <> la[Length(la)]) then
+    If (Length(la[i]) <= minLength) AND NOT(la[i] = la[Length(la)]) then
+    begin
+      minLength := Length(la[i]);
       s1 := la[i];
-
-  end;
-
-
+    end;
+  WriteLn(s1);
 end.
