885ac53c6e585a221ba21b67964bfb9e5bdf6d43
Tatsiana Marfel <tatsiana_marfel@epam.com>
2018-05-15 09:40:02 +0300

    Add word count and comments

--- lab2.pas
+++ lab2.pas
@@ -9,39 +9,81 @@ uses regexpr;
 
 var
   re : TRegExpr;
-  s1, s2, ls : string;
+    {экземпляр класса TRegExpr}
+  ls, s1, s2 : string;
+    {исходная и итоговые строки}
   la : array of string;
-  i, minLength, ind : Integer;
-
+    {массив для добавления каждого слова строки}
+  minLength, i, ind, count : Integer;
+    {Вспомогательные переменные для подсчета минимальной длины и итерации  цикла}
+  word : boolean;
 begin
-  ReadLn(ls);
-  if Length(ls) < 1 then
+  ReadLn(ls); {Чтение данных введенных пользователем}
+  if Length(ls) < 1 then {Условие для обработки пустой строки}
     WriteLn('Error. The string should not be empty');
-  re := TRegExpr.Create('[\s]{1,2}');
-  SetLength(la, 90);
-  ind := 1;
-  for i:= 1 to Length(ls) do
+  re := TRegExpr.Create; {Создание экземляра класса TRegExpr для обработки регулярных выражений}
+  re.Expression := ('[\s,.]'); {Набор символов для разбиения строки на слова}
+
+  ind := 0; {Установление индекса для первого элемента массива}
+  count := 0; {Инициализация переменной для подсчета слов в строке}
+
+  while i < Length(ls) do
   begin
-    if NOT(re.exec(ls[i])) then
+    if re.exec(ls[i]) then
+      word := false
+    else if NOT(word) then
+    begin
+      word := true;
+      count := count + 1;
+    end;
+    i := i + 1;
+  end;
+  SetLength(la, count);
+
+  for i:=0 to Length(ls) do
+  begin
+    if NOT re.exec(ls[i]) then
     begin
       s1 := Concat(s1 + ls[i]);
     end
     else
     begin
       la[ind] := s1;
+      WriteLn(s1);
       s1 := '';
-      WriteLn(Length(la[ind]));
-      ind:=ind+1;
-    end;
 
+      WriteLn(Length(la[ind]));
+      ind := ind + 1;
+    end;
   end;
-  minLength:=Length(la[1]);
-  WriteLn('minLength', minLength);
-  for i:=1 to Length(la) do
-    If (Length(la[i]) <= minLength) AND NOT(la[i] = la[Length(la)]) then
+
+
+  minLength := Length(la[0]);
+  for i:=0 to Length(la) do
+
+  begin
+
+    If (Length(la[i]) < minLength) AND NOT(la[i] = la[Length(la)]) then
     begin
       minLength := Length(la[i]);
       s1 := la[i];
     end;
+
+  end;
   WriteLn(s1);
+
+  for i:=0 to Length(la) do
+  begin
+    if pos('th', la[i]) <> 0 then
+    begin
+      Delete(la[i], pos('th', la[i]), 2);
+      Writeln(la[i]);
+    end
+    else
+    begin
+      WriteLn('No occurrences of "th" were found. Try again with a differnt string.');
+      break
+    end;
+  end;
+
 end.
