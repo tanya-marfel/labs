62c13d39f838311b4e70333567d6774663a25775
Tatsiana Marfel <tatsiana_marfel@epam.com>
2018-05-30 23:43:20 +0300

    Add task 1 draft

--- PerfectNumbers.pas
+++ PerfectNumbers.pas
@@ -9,22 +9,19 @@ var
 begin
   readln(m);
   ReadLn(n);
-  sum := 0;
+  //sum := 0;
   for j:=m to n do
   begin
     for i:=1 to j - 1 do
     begin
-      //WriteLn(j);
       if (j mod i) = 0 then
       begin
         sum := sum + i;
       end;
-      //WriteLn(sum);
     end;
     if (sum = j) then
     begin
       WriteLn(sum);
-
     end;
     sum := 0;
   end;
